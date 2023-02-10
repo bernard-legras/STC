89be0b286d81f60cf6e720ce51110b8c78b68137
Bernard Legras <bernard.legras@lmd.ipsl.fr>
2023-02-10 16:19:58 +0100

    Change from ciclad to spirit in host detection.

--- pylib/STCdata.py
+++ pylib/STCdata.py
@@ -16,7 +16,7 @@ import socket
 
 if socket.gethostname() == 'Graphium':
     STC_database = 'C:\\cygwin64\\home\\berna\\data\\STC\\STC-M55\\database'
-elif 'ciclad' in socket.gethostname():
+elif 'spirit' in socket.gethostname():
     STC_database ='/home/legras/STC/STC-M55/database'
 elif ('climserv' in socket.gethostname()) | ('polytechnique' in socket.gethostname()):
     STC_database = '/home/legras//STC/STC-M55/database'
