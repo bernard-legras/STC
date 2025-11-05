c98fec140e08503268636940ab82538172de5e59
Bernard Legras <bernard.legras@lmd.ipsl.fr>
2025-11-05 21:42:35 +0100

    Adjustment of estar in the ice formula of Emanuel (factor 1.021). New ice saturation function after Murphy and Koop.

--- pylib/satratio.py
+++ pylib/satratio.py
@@ -10,15 +10,16 @@ Liquid formula added on 12/12/2022
 @author: Bernard Legras
 """
 import numpy as np
-# Calculation of the saturation mixing ratio from actual temperature and pressure
+# Calculation of the saturation mass mixing ratio from actual temperature and pressure
 def satratio(p,T):
-    """ Calculate the mass saturation ratio from pressure (in Pa) and temperature 
-    (in K). Output in kg/kg """
+    """ Calculate the mass saturation ratio from pressure (in Pa) and temperature
+    (in K). Output in kg/kg.Given by Emanuel to be valid up to 0.3% over the range 190K-273K
+    """
     estar = 1.0008*np.exp(23.33086-(6111.72784/T)+0.15215*np.log(T))
-    satr = 0.622 * estar/(0.01*p-estar)
-    return satr
+    satr = 1.0021 * 0.622 * estar/(0.01*p-estar)
+    return satr, estar*100
 def liquid_satratio(p,T):
-    """ Calculate the mass saturation ratio from pressure (in Pa) and temperature 
+    """ Calculate the mass saturation ratio from pressure (in Pa) and temperature
     (in K). Output in kg/kg """
     estar = 6.112 * np.exp(17.67*(T-273.15)/(T-29.65))
     satr = 0.622 * estar/(0.01*p-estar)
@@ -33,3 +34,28 @@ def liquid_satratio_2(p,T):
     vs = np.exp( (a/T) + b + (c*T) + (d*T**2) + e*np.log(T))
     satr = 0.622 * vs/(p-vs)
     return satr
+def esati_murphy(p, T):
+    """ei in Pa saturation vapor pressure with respect to hexagonal (most stable) ice
+       Murphy and Koop 2005, QJRMS
+       Input in Pa and K
+       Output in kg/kg """
+    lnP = 9.550426-5723.265/T+3.53068*np.log(T)-0.00728332*T
+    estar = np.exp(lnP)
+    esati_murphy = estar / (p-estar)
+    #: Convert to kg/kg
+    esati_murphy = esati_murphy * 0.622
+    #ppmv
+    #kg
+    return esati_murphy, estar
+
+#%% Tests in the main program
+""" The values of the tests are taken from the table C1 of Murphy & Koop, 2005,
+    QJRMS. """
+if __name__ == '__main__':
+    Tl = (150, 180, 210, 240, 273.15, 273.16)
+    pl = 100
+    for T in Tl:
+        rvs1,estar = esati_murphy(pl, T)
+        print('MK {:.2f} {:.4E}'.format(T, estar))
+        rvs2,estar = satratio(pl,T)
+        print('KE {:.2f} {:.4E}'.format(T, estar))
